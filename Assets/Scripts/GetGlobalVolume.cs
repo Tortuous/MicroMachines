1047efe9e7b95e262a2d7a123714d15ac781b758
Ryad Shaanbi <ryad_shaanbi@hotmail.com>
2017-05-04 13:08:58 +0200

    Getting global sound that updates by slider in 1 scene

---
+++ Assets/Scripts/GetGlobalVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGlobalVolume : MonoBehaviour {
+    
+    private float globalVolume;
+    public void Update()
+    {
+        globalVolume = PlayerPrefs.GetFloat("GlobalVolume");
+        gameObject.GetComponent<AudioSource>().volume = globalVolume;
+    }
+}
---
+++ Assets/Scripts/SetGlobalVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetGlobalVolume : MonoBehaviour {
+    public float curVolume;
+
+    public void Update()
+    {
+        curVolume = gameObject.GetComponent<Slider>().value;
+    }
+    public void VolumeInput()
+    {
+        PlayerPrefs.SetFloat("GlobalVolume", curVolume);
+    }
+    public void Awake()
+    {
+        curVolume = PlayerPrefs.GetFloat("GlobalVolume");
+    }
+}
