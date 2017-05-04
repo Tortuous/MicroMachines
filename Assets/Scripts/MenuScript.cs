cc36e4783c56da45e24902a8e38497a0cb2da7d3
Ryad Shaanbi <ryad_shaanbi@hotmail.com>
2017-05-04 13:54:15 +0200

    Changing auiosource in all scenes

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,9 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public AudioSource audiosource;
+    public AudioClip audioclipMenu;
+    public AudioClip audioclipGame;
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        if(sceneName == "Track1")
+        {
+            audiosource.clip = audioclipGame;
+        }
+        if(sceneName == "Track2")
+        {
+            audiosource.clip = audioclipGame;
+        }
+        if(sceneName == "MainMenu")
+        {
+            audiosource.clip = audioclipMenu;
+        }
+        if(sceneName == "Options")
+        {
+            audiosource.clip = audioclipMenu;
+        }
     }
 
     public void ExitGame()
